32fd1bc533f7c8ee2390fc143d3aa662c9e2a3b1
Adrian Carolli <adrian.caarolli@gmail.com>
2021-07-07 18:47:19 -0400

    play styles

--- screens/Play/index.tsx
+++ screens/Play/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, ScrollView, TouchableWithoutFeedback } from "react-native";
+import { Image, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { AntDesign as Icon } from "@expo/vector-icons";
 
 import { View, Text } from "../../components/Themed";
@@ -40,15 +40,13 @@ export default function PlayScreen({ route }: Props) {
     return <NotFoundScreen />;
   }
 
-  const { title } = meditation;
+  const { title, subtitle, image } = meditation;
 
   return (
-    <ScrollView
-      contentContainerStyle={styles.contentContainer}
-      style={styles.container}
-      showsVerticalScrollIndicator={false}
-    >
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <Image source={image} style={styles.image} />
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.subtitle}>{subtitle}</Text>
       <View style={styles.controls}>
         <PlayerIcon name="stepbackward" onPress={() => {}} size={20} />
         {isPlaying ? (
@@ -58,25 +56,40 @@ export default function PlayScreen({ route }: Props) {
         )}
         <PlayerIcon name="stepforward" onPress={() => {}} size={20} />
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 36,
-    paddingBottom: 36,
+    paddingTop: 66,
+    paddingBottom: 66,
     paddingLeft: 31,
     paddingRight: 31,
     backgroundColor: Colors.light.background,
   },
-  contentContainer: {
-    paddingBottom: 36,
+  title: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 16,
+    marginBottom: 50,
+  },
+  image: {
+    width: 252,
+    height: 252,
+    marginBottom: 66,
+    borderRadius: 10,
+    alignSelf: "center",
   },
   controls: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-evenly",
   },
 });

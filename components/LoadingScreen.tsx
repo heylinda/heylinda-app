3d131c150f3e2aa24dafe55ce001d4beaafe0744
Adrian Carolli <adrian.caarolli@gmail.com>
2021-07-12 14:41:48 -0400

    use primary color

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import { StyleSheet } from 'react-native'
 import { ActivityIndicator } from 'react-native-paper'
+import { useThemeColor } from './Themed'
 
 interface Props {
   loading: boolean
 }
 export const LoadingScreen: React.FC<Props> = ({ loading }) => {
+  const primary = useThemeColor({}, 'primary')
+
   if (loading) {
-    return <ActivityIndicator style={styles.loading} />
+    return <ActivityIndicator style={styles.loading} color={primary} />
   }
   return null
 }

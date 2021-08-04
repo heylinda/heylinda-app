be7c451d1328106bc71d6e2e67ca52da3bbc9b8e
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-08-04 16:02:45 -0600

    adds AppRoute enum for navigation

---
+++ navigation/AppRoutes.ts
@@ -0,0 +1,13 @@
+export enum AppRoutes {
+  Root = 'Root',
+  NotFound = 'NotFound',
+  Main = 'Main',
+  CompletedScreen = 'CompletedScreen',
+  Home = 'Home',
+  Stats = 'Stats',
+  Settings = 'Settings',
+  HomeScreen = 'HomeScreen',
+  PlayScreen = 'PlayScreen',
+  StatsScreen = 'StatsScreen',
+  SettingsScreen = 'SettingsScreen',
+}

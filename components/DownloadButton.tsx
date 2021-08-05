1567af4bfd1a7457a8955611f98666bb8d90f2a8
Trent <trent@mercatureweb.com>
2021-08-05 14:51:35 -0400

    Synchronized redux state with filesystem downloads

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -1,64 +1,71 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { AntDesign as Icon } from '@expo/vector-icons'
-import { useMeditation } from '../hooks'
 import * as FileSystem from 'expo-file-system'
+
+import { useMeditation, useAppDispatch } from '../hooks'
+import { filepaths } from '../redux/meditationSlice'
+import { store } from '../redux/store'
+import { useFiles } from '../hooks/useFiles'
 
 export default function DownloadButton(props: any) {
   const id = props.id
   const meditation = useMeditation(id)
+  const files = useFiles('.mp3')
   const uri = meditation?.uri || ''
+  const [audioFiles, setAudioFiles] = useState<string[]>([])
   const [downloaded, setDownloaded] = useState(false)
+  const dispatch = useAppDispatch()
 
-  const filename = () => {
-    let filename = uri.split('/').pop()
+  const filename = (path: string) => {
+    let filename = path.split('/').pop()
     if (!filename) {
       return
     }
     return filename
   }
 
-  const removeAudioFile = async () => {
-    let base = await FileSystem.documentDirectory
-    if (!base) {
-      return
+  useEffect(() => {
+    if (audioFiles.length === 0) {
+      // Fill redux state .mp3 filepaths on device
+      files.then((paths) => {
+        dispatch(filepaths(paths))
+        let storedFiles = store.getState().meditation.filepaths
+        setAudioFiles(storedFiles)
+      })
     }
 
-    FileSystem.deleteAsync(base + filename()).then(() => {
-      setDownloaded(false)
-      // Remove .mp3 from filesystem and the filepath from redux-persist
-    })
-  }
+    if (!downloaded) {
+      if (meditation && audioFiles) {
+        let name = filename(meditation.uri) ?? ''
+        setDownloaded(audioFiles.includes(name))
+      }
+    }
+  }, [audioFiles, files, downloaded, dispatch, meditation])
 
   const saveAudioFile = async () => {
     let base = await FileSystem.documentDirectory
-    if (!base) {
+    if (!base || !meditation) {
       return
     }
 
-    FileSystem.downloadAsync(uri, base + filename()).then((res) => {
-      if (res.status == 200) {
+    const path = base + filename(meditation.uri)
+
+    FileSystem.downloadAsync(uri, path).then((res) => {
+      if (res.status === 200) {
+        dispatch(filepaths(path))
         setDownloaded(true)
-        // Store the device filepath to the mp3 in redux-persist
       }
     })
   }
 
   if (downloaded) {
-    return (
-      <Icon
-        name="checkcircleo"
-        style={{ marginTop: 10 }}
-        size={25}
-        color="black"
-        onPress={removeAudioFile}
-      />
-    )
+    return <Icon name="checkcircleo" style={{ marginTop: 10 }} size={15} color="black" />
   } else {
     return (
       <Icon
         name="download"
         style={{ marginTop: 10 }}
-        size={25}
+        size={15}
         color="black"
         onPress={saveAudioFile}
       />

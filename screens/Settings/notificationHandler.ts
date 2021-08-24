26767ed98833ece242faec98d13f9c3ea7112bb1
SalikSayyed <51844798+SalikSayyed@users.noreply.github.com>
2021-08-24 19:02:14 +0530

    Notification Cleaner

--- screens/Settings/notificationHandler.ts
+++ screens/Settings/notificationHandler.ts
@@ -1,88 +1,76 @@
-import * as Notifications from "expo-notifications";
-import * as Permissions from "expo-permissions";
-import { Platform } from "react-native";
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
+import { Platform } from 'react-native'
 
 const askPermissions = async () => {
-  const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-  let finalStatus = existingStatus;
-  if (existingStatus !== "granted") {
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    finalStatus = status;
+  const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS)
+  let finalStatus = existingStatus
+  if (existingStatus !== 'granted') {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+    finalStatus = status
   }
-  if (finalStatus !== "granted") {
-    return false;
+  if (finalStatus !== 'granted') {
+    return false
   }
-  return true;
-};
-
-
-const deleteNotifications = async()=>{
-    Notifications.dismissAllNotificationsAsync();
+  return true
 }
 
-
-
-const iosScheduler = (weekday:number[],time:Date)=>{
-   
-if(weekday!=[-1]){
-  Notifications.cancelAllScheduledNotificationsAsync();
- weekday.forEach((v)=>{
-     if(v!=-1){
-     Notifications.scheduleNotificationAsync({
-        content: {
-          title: 'Practice your meditation today  🙇‍♂️',
-          
-        },
-        trigger:{
-          hour:time.getHours(),
-          minute:time.getMinutes(),
-          weekday : v
-        }
-      });
-    }
- });
-}
+const deleteNotifications = async () => {
+  Notifications.cancelAllScheduledNotificationsAsync()
 }
 
-
-
-  const AndroidSchedular = (weekday:number[],time:Date)=>{
-   
-      Notifications.cancelAllScheduledNotificationsAsync();
-    if(weekday!=[-1]){
-
-     weekday.forEach((v)=>{
-         if(v!=-1){
-         Notifications.scheduleNotificationAsync({
-            content: {
-              title: 'Practice your meditation today 🙇‍♂️',
-             
-            },
-            trigger : {
-              hour: time.getHours(),
-              minute: time.getMinutes(),
-              weekday: v,
-              repeats:true
-          }
-          });
-        }
-
-     }
-     
-     );
-
-    }
+const iosScheduler = (weekday: number[], time: Date) => {
+  if (weekday != [-1]) {
+    Notifications.cancelAllScheduledNotificationsAsync()
+    weekday.forEach((v) => {
+      if (v != -1) {
+        Notifications.scheduleNotificationAsync({
+          content: {
+            title: 'Practice your meditation today  🙇‍♂️',
+          },
+          trigger: {
+            hour: time.getHours(),
+            minute: time.getMinutes(),
+            weekday: v,
+          },
+        })
+      }
+    })
   }
-
-
-const PlatformScheduler =(weekday:number[],time:Date)=>{
-  if(Platform.OS=='android'){
-    AndroidSchedular(weekday,time);
-  }
-  if(Platform.OS=='ios'){
-    iosScheduler(weekday,time);
-  }
-  
 }
 
-export default {askPermission : askPermissions,testSchedular : PlatformScheduler,deleteNotification : deleteNotifications};
+const AndroidSchedular = (weekday: number[], time: Date) => {
+  Notifications.cancelAllScheduledNotificationsAsync()
+  if (weekday != [-1]) {
+    weekday.forEach((v) => {
+      if (v != -1) {
+        Notifications.scheduleNotificationAsync({
+          content: {
+            title: 'Practice your meditation today 🙇‍♂️',
+          },
+          trigger: {
+            hour: time.getHours(),
+            minute: time.getMinutes(),
+            weekday: v,
+            repeats: true,
+          },
+        })
+      }
+    })
+  }
+}
+
+const PlatformScheduler = (weekday: number[], time: Date) => {
+  if (Platform.OS == 'android') {
+    AndroidSchedular(weekday, time)
+  }
+  if (Platform.OS == 'ios') {
+    iosScheduler(weekday, time)
+  }
+}
+
+export default {
+  askPermission: askPermissions,
+  testSchedular: PlatformScheduler,
+  deleteNotification: deleteNotifications,
+}

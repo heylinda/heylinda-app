a97e1f1f2189775ee23aec7134587e7e5058925f
Emerson Pinheiro <e_pms@hotmail.com>
2021-10-06 19:02:37 -0300

    add comment in useMinutesToStatsTime hook

--- hooks/useMinutesToStatsTime.ts
+++ hooks/useMinutesToStatsTime.ts
@@ -1,3 +1,4 @@
+/** Receives a number of minutes and return a friendly string for easier reading */
 export function useMinutesToStatsTime(minutes: number) {
   const h = Math.floor(minutes / 60)
   const m = minutes % 60

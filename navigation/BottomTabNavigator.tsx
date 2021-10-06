ccd53ca64f0bb7d3dc071a9886b45c067c7447c5
Pedro Pessoa <pedro.pessoa@wolt.com>
2021-10-06 03:26:39 +0300

    fix: header of tab navigator showing twice

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -20,7 +20,7 @@ export default function BottomTabNavigator() {
   return (
     <BottomTab.Navigator
       initialRouteName="Home"
-      screenOptions={{ tabBarActiveTintColor: Colors[colorScheme].tint }}
+      screenOptions={{ tabBarActiveTintColor: Colors[colorScheme].tint, headerShown: false }}
     >
       <BottomTab.Screen
         name="Home"

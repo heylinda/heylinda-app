b1cd84774513b2f186107d67119784a80c1f1532
craigrobertson <robertson.cra@gmail.com>
2021-10-31 14:10:26 +0000

    update tests

--- cypress/integration/HomeScreen.ts
+++ cypress/integration/HomeScreen.ts
@@ -5,7 +5,6 @@ describe('HomeScreen', () => {
     cy.contains('Home')
 
     cy.contains('POPULAR')
-
     cy.contains('Power of Love')
     cy.contains('Love and Peace')
 
@@ -17,6 +16,26 @@ describe('HomeScreen', () => {
 
     cy.contains('Yawn and Stretch')
     cy.contains('Rise and Shine')
+
+    cy.contains('ANXIETY')
+    cy.contains('Deep and Quick Relaxation')
+    cy.contains('Love and Peace')
+
+    cy.contains('Calming Medition')
+    cy.contains('Deep Relaxation')
+
+    cy.contains('Candle Relaxation')
+    cy.contains('Get Some Rest')
+
+    cy.contains('SLEEP')
+    cy.contains('Deep Sleep')
+    cy.contains('Wake Up Refreshed')
+
+    cy.contains('Short Sleep')
+    cy.contains('For Taking a Nap')
+
+    cy.contains('Good Sleep')
+    cy.contains('Drift Off To Sleep')
   })
 
   it('can save favourite meditations to home screen', () => {

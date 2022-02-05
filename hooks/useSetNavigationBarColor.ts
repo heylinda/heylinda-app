0089a4006a2eff138bae6a735b1d7cb6686f86c3
Adrian Carolli <adrian.caarolli@gmail.com>
2022-02-05 09:19:17 -0500

    fix: supress warning on non-android devices

--- hooks/useSetNavigationBarColor.ts
+++ hooks/useSetNavigationBarColor.ts
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import * as NavigationBar from 'expo-navigation-bar'
 import { useThemeColor } from '../components'
+import { Platform } from 'react-native'
 
 export default function useSetNavigationBarColor() {
   const backgroundColor = useThemeColor({}, 'navBarBackground')
 
   React.useEffect(() => {
-    NavigationBar.setBackgroundColorAsync(backgroundColor)
+    if (Platform.OS === 'android') {
+      NavigationBar.setBackgroundColorAsync(backgroundColor)
+    }
   }, [backgroundColor])
 }

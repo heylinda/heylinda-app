cd9ec01a6117bfbb385e8b73c9e334450b60b208
craigrobertson <robertson.cra@gmail.com>
2022-02-07 19:51:50 +0000

    remove card component

--- screens/Settings/About/index.tsx
+++ screens/Settings/About/index.tsx
@@ -1,30 +1,22 @@
 import * as React from 'react'
 import * as Application from 'expo-application'
-import { Caption, Card, List } from 'react-native-paper'
+import { Caption, Divider, List } from 'react-native-paper'
 
 const About = () => {
   return (
     <>
-      <Card>
-        <Card.Content>
-          <List.Item
-            title="Application Version"
-            right={() => (
-              <Caption style={{ marginTop: 5 }}>{Application.nativeApplicationVersion}</Caption>
-            )}
-          />
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <List.Item
-            title="Build Version"
-            right={() => (
-              <Caption style={{ marginTop: 5 }}>{Application.nativeBuildVersion}</Caption>
-            )}
-          />
-        </Card.Content>
-      </Card>
+      <List.Item
+        title="Application Version"
+        right={() => (
+          <Caption style={{ marginTop: 5 }}>{Application.nativeApplicationVersion}</Caption>
+        )}
+      />
+      <Divider />
+      <List.Item
+        title="Build Version"
+        right={() => <Caption style={{ marginTop: 5 }}>{Application.nativeBuildVersion}</Caption>}
+      />
+      <Divider />
     </>
   )
 }

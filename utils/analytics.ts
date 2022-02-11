73348b721df3118917adfc0e2b3a3ac633e06ebe
Adrian Carolli <adrian.caarolli@gmail.com>
2022-02-11 17:14:22 -0500

    feat: adds amplitude analytics tracking (#144)
    * feat: adds amplitude analytics tracking
    
    Removes google analytics tracking in favour of amplitude
    
    * fix: do not run on web

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,5 +1,14 @@
-import { Analytics } from 'expo-analytics'
+import * as Amplitude from 'expo-analytics-amplitude'
+import { Platform } from 'react-native'
 
-const analytics = new Analytics('UA-219496436-2')
+export const initializeAnalytics = async () => {
+  if (Platform.OS !== 'web') {
+    await Amplitude.initializeAsync('c53c4e54414340dc1e6feeb7fd95293c')
+  }
+}
 
-export default analytics
+export const logEvent = async (eventName: string) => {
+  if (Platform.OS !== 'web') {
+    await Amplitude.logEventAsync(eventName)
+  }
+}

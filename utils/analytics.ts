1473cb0485ff5fe5d65aef0f64e7190eaaea5a53
Adrian Carolli <adrian.caarolli@gmail.com>
2022-02-11 16:56:36 -0500

    fix: do not run on web

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,9 +1,14 @@
 import * as Amplitude from 'expo-analytics-amplitude'
+import { Platform } from 'react-native'
 
 export const initializeAnalytics = async () => {
-  await Amplitude.initializeAsync('c53c4e54414340dc1e6feeb7fd95293c')
+  if (Platform.OS !== 'web') {
+    await Amplitude.initializeAsync('c53c4e54414340dc1e6feeb7fd95293c')
+  }
 }
 
 export const logEvent = async (eventName: string) => {
-  await Amplitude.logEventAsync(eventName)
+  if (Platform.OS !== 'web') {
+    await Amplitude.logEventAsync(eventName)
+  }
 }

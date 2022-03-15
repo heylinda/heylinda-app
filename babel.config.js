5fda80ccc93b8c7e82735e1c03cc0fd1c6d27f70
Emerson Pinheiro <e_pms@hotmail.com>
2022-03-15 16:07:32 -0300

    refact: replaces hard-coded api url by API_URL env variable chore: add react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+        },
+      ],
+    ],
   }
 }
---
+++ env.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-native-dotenv' {
+  export const API_URL: string
+}

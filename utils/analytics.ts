a5a268fa3111b64670c2f58c519ef14c644ad8d8
Emerson Pinheiro <e_pms@hotmail.com>
2022-03-15 16:19:04 -0300

    refact: adds UA to an env variable

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,9 +1,10 @@
 import * as Amplitude from 'expo-analytics-amplitude'
 import { Platform } from 'react-native'
+import { UA } from 'react-native-dotenv'
 
 export const initializeAnalytics = async () => {
   if (Platform.OS !== 'web') {
-    await Amplitude.initializeAsync('c53c4e54414340dc1e6feeb7fd95293c')
+    await Amplitude.initializeAsync(UA)
   }
 }
 

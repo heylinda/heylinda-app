f459af20cae8f03e176b3b1e8f255a28016ed0e1
Adrian Carolli <adrian.caarolli@gmail.com>
2022-04-01 18:59:06 -0400

    feat: adds transparent tracking for iOS

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,13 +1,26 @@
 import * as Amplitude from 'expo-analytics-amplitude'
 import { Platform } from 'react-native'
+import { requestTrackingPermissionsAsync } from 'expo-tracking-transparency'
 
 export const initializeAnalytics = async () => {
+  if (Platform.OS === 'ios') {
+    const { status } = await requestTrackingPermissionsAsync()
+    if (status !== 'granted') {
+      return
+    }
+  }
   if (Platform.OS !== 'web') {
     await Amplitude.initializeAsync('c53c4e54414340dc1e6feeb7fd95293c')
   }
 }
 
 export const logEvent = async (eventName: string) => {
+  if (Platform.OS === 'ios') {
+    const { status } = await requestTrackingPermissionsAsync()
+    if (status !== 'granted') {
+      return
+    }
+  }
   if (Platform.OS !== 'web') {
     await Amplitude.logEventAsync(eventName)
   }
